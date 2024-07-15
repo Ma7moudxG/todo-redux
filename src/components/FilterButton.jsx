3d7ca24875d88230cf9091277998c8daf89383d7
Ma7moudxG <132134084+Ma7moudxG@users.noreply.github.com>
2024-07-15 23:27:11 +0300

    Search + Filter are done

--- src/components/FilterButton.jsx
+++ src/components/FilterButton.jsx
@@ -1,8 +1,30 @@
+import { useDispatch, useSelector } from "react-redux"
+import { filterTodos, markAllCommpleted } from "../redux/actions";
 
 
 const FilterButton = () => {
+    const dispatch = useDispatch()
+
+    const currentFilter = useSelector((state) => state.filter);
+    const handleFilter = (filterItem) => {
+        dispatch(filterTodos(filterItem))
+    }
   return (
-    <div>FilterButton</div>
+    <div className="flex items-center mb-2 md:mb-0">
+        <select 
+            value={currentFilter}
+            onChange={(e) => handleFilter(e.target.value)}
+            className="bg-white text-sm px-2 py-2 rounded 
+            border-b-2 border-gray-300 focus:outline-none w-[100px] md:w-[200px]">
+            <option value="ALL">Default</option>
+            <option value="COMPLETED">Completed</option>
+            <option value="INCOMPLETE">Incomplete</option>
+        </select>
+
+        <button 
+        onClick={() => dispatch(markAllCommpleted())}
+        className="text-sm px-3 py-2 bg-purple-500 text-white ml-3 rounded">Mark All Completed</button>
+    </div>
   )
 }
 
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { BsPlus, BsSearch } from "react-icons/bs";
 import { useDispatch } from "react-redux";
 import { addTodo, updateSearchTerm } from "../redux/actions";
+import FilterButton from "./FilterButton";
+import TodoList from "./TodoList";
 
 const Todo = () => {
   const [newTodoText, setNewTodoText] = useState("");
@@ -17,8 +19,8 @@ const Todo = () => {
 
   const handleSearch = (value) => {
     setSearchTerm(value);
-    dispatch(updateSearchTerm(value))
-  }
+    dispatch(updateSearchTerm(value));
+  };
   return (
     <div className="max-w-4xl mx-auto sm:mt-8 p-4 bg-gray-100 rounded">
       <h2 className="mt-3 mb-6 text-2xl font-bold text-center uppercase">
@@ -35,11 +37,11 @@ const Todo = () => {
           id="addTodoInput"
           placeholder="Add Todo"
           className="flex-grow p-2 border-b-2 border-gray-300 
-          focus:outline-none focus:border-blue-500"
+          focus:outline-none focus:border-blue-500 rounded"
         />
         <button
           onClick={handleAddTodo}
-          className="bg-blue-500 p-2 ml-4 text-white rounded 
+          className="bg-blue-500 p-3 px-6 ml-4 text-white rounded 
             hover:bg-blue-700 focus:outline-none"
         >
           <BsPlus className="text-white w-full" />
@@ -47,27 +49,31 @@ const Todo = () => {
       </div>
 
       {/* filter and search */}
-      <div>
+      <div className="flex items-center justify-between flex-col md:flex-row">
+        <FilterButton />
         <div className="flex items-center mb-4">
-        <input
-          value={searchTerm}
-          onChange={(e) => handleSearch(e.target.value)}
-          type="text"
-          name="addTodoInput"
-          id="addTodoInput"
-          placeholder="Add Todo"
-          className="flex-grow p-2 border-b-2 border-gray-300 
-          focus:outline-none focus:border-blue-500"
-        />
-        <button
-          onClick={handleAddTodo}
-          className="bg-blue-500 p-2 ml-4 text-white rounded 
+          <input
+            value={searchTerm}
+            onChange={(e) => handleSearch(e.target.value)}
+            type="text"
+            name="addTodoInput"
+            id="addTodoInput"
+            placeholder="Search Todo"
+            className="flex-grow p-2 border-b-2 border-gray-300 
+          focus:outline-none focus:border-blue-500 rounded"
+          />
+          <button
+            onClick={handleAddTodo}
+            className="bg-blue-500 p-3 px-6 ml-4 text-white rounded 
             hover:bg-blue-700 focus:outline-none"
-        >
-          <BsSearch className="text-white w-full" />
-        </button>
+          >
+            <BsSearch className="text-white w-full" />
+          </button>
         </div>
       </div>
+
+      {/* Todo List */}
+      <TodoList />
     </div>
   );
 };
